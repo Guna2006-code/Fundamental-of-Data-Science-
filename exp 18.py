c5e870c4b1aafb9dfda2e13ffaf560fe926f85d1
Guna2006-code <guna994047@gmail.com>
2026-03-09 10:17:42 +0530

    Delete exp 18.py

--- exp 18.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import Perceptron
-from sklearn.datasets import load_iris
-
-data=load_iris()
-
-model=Perceptron()
-model.fit(data.data,data.target)
-
-print(model.predict([data.data[0]]))
